726599101afc67a02e4539f19508a7c145312e4c
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-04-16 18:33:31 +0200

    Remove unneeded file

--- pravopis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python ~/pwb/scripts/replace.py -always -ns:0 -search:vyjímk 'vyjímk' 'výjimk' -summary:Pravopis
-python ~/pwb/scripts/replace.py -always -regex -ns:0 -start:! "[Čč]esk(á|é|ou) Republi(ce|k[auy]|kou)" "Česk\1 republi\2" -summary:Pravopis
