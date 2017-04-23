14400f132834d00f154cde073a8bb7025325989b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-04-23 13:16:34 +0200

    Fix

--- cleanSandbox.sh
+++ cleanSandbox.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python ~/pwb/scripts/clean_sandbox.py -family:wikipedia -lang:cs
+export PYTHONPATH=/shared/pywikipedia/core:/shared/pywikipedia/core/externals/httplib2:/shared/pywikipedia/core/scripts
+python ~/pwb/scripts/clean_sandbox.py -family:wikipedia -lang:cs -hours:12
