ff15ad1d762fa399d1ae8a53b78f583dc7f3c821
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-08-06 01:24:27 +0200

    Execute addPrekladsCT

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 export LC_ALL=en_US.UTF-8
 /data/project/urbanecmbot/11bots/standardization.sh
 python /data/project/urbanecmbot/exportWikidataModule/export.py &>> /data/project/urbanecmbot/logs/exportWD.log
+bash /data/project/urbanecmbot/addPrekladCT/run.sh
