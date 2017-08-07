c9af49c2c496ba9c373d9b50bdb8adc8799fa04d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-08-07 16:17:04 +0200

    +wikidataCoorImport

--- daily.sh
+++ daily.sh
@@ -2,3 +2,4 @@ export LC_ALL=en_US.UTF-8
 /data/project/urbanecmbot/11bots/standardization.sh
 python /data/project/urbanecmbot/exportWikidataModule/export.py &>> /data/project/urbanecmbot/logs/exportWD.log
 bash /data/project/urbanecmbot/addPrekladCT/run.sh
+bash /data/project/urbanecmbot/wikidataCoorImport/run.sh
