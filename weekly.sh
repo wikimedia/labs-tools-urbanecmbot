593b3a09146c2fc1fa102222c1122a46a5abeba6
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-18 11:27:02 +0100

    Rename to weekly

--- weekly.sh
+++ weekly.sh
@@ -4,5 +4,5 @@ export LC_ALL=en_US.UTF-8
 python /shared/pywikipedia/core/scripts/archivebot.py Archivace &>> /data/project/urbanecmbot/logs/archivePages.log
 python /data/project/urbanecmbot/11bots/did_you_know.py -current &>> /data/project/urbanecmbot/logs/did_you_know.log
 cd /data/project/urbanecmbot/autoprotect
-python autoprotect.py
+python weekly.py
 cd $OLDPWD
