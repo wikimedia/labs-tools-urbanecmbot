3f1520793461f2469de144ee6615304bae2d99c9
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-18 11:29:10 +0100

    Run daily.py from autoprotect daily

--- daily.sh
+++ daily.sh
@@ -9,3 +9,6 @@ python /data/project/urbanecmbot/orphan/to_delete_orphan.py
 python /data/project/urbanecmbot/ukoly/addPriority.py
 python /data/project/urbanecmbot/ukoly/bezPodstranky.py
 python /data/project/urbanecmbot/mostLinkedDisambigs/mostLinkedDisambigs.py
+cd /data/project/urbanecmbot/autoprotect
+python daily.py
+cd $OLDPWD
