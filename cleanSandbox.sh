0148b17de23a3f666b150cfab2eec8c7a0f29115
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-02-22 14:59:17 +0100

    Remove clenaSandbox

--- cleanSandbox.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=/shared/pywikipedia/core:/shared/pywikipedia/core/externals/httplib2:/shared/pywikipedia/core/scripts
-python ~/pwb/scripts/clean_sandbox.py -family:wikipedia -lang:cs -hours:1
