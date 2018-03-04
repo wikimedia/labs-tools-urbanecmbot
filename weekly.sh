03f0353cf2cd4034869b3b81aa711ccfe66f9b12
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-03-04 15:24:27 +0100

    Do not use strange cd syntax, let's name path absolutely

--- weekly.sh
+++ weekly.sh
@@ -3,6 +3,4 @@
 export LC_ALL=en_US.UTF-8
 python /shared/pywikipedia/core/scripts/archivebot.py Archivace &>> /data/project/urbanecmbot/logs/archivePages.log
 python3 /data/project/urbanecmbot/11bots/did_you_know.py -current &>> /data/project/urbanecmbot/logs/did_you_know.log
-cd /data/project/urbanecmbot/autoprotect
-python weekly.py
-cd $OLDPWD
+cd /data/project/urbanecmbot/autoprotect && python weekly.py && cd /data/project/urbanecmbot/11bots
