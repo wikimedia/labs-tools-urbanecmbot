a3d339ab815fbb1c3e88c2ae5727716c3f89ffa3
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-04-04 08:39:37 +0200

    Remove daily/hourly/weekly, no longer works

--- hourly.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LC_ALL=en_US.UTF-8
-python /data/project/urbanecmbot/mark-students/cache.py # Regenerate stylesheet that greens students
-PYWIKIBOT2_DIR=/data/project/urbanecmbot/11bots/sysopbots python /data/project/urbanecmbot/11bots/sysopbots/purgeKonecMazani.py
--- weekly.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export LC_ALL=en_US.UTF-8
-python /shared/pywikipedia/core/scripts/archivebot.py Archivace &>> /data/project/urbanecmbot/logs/archivePages.log
-python3 /data/project/urbanecmbot/11bots/did_you_know.py -current &>> /data/project/urbanecmbot/logs/did_you_know.log
-PYWIKIBOT2_DIR=/data/project/urbanecmbot/autoprotect python /data/project/urbanecmbot/autoprotect/weekly.py
