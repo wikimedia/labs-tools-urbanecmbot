8106dc64c778223378fd4661b249ada64d1b7005
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-08 13:18:29 +0200

    Run patrol.py

--- cswiki/userbots/patrolAutopatrolled/run.sh
+++ cswiki/userbots/patrolAutopatrolled/run.sh
@@ -2,4 +2,5 @@
 
 cd /data/project/urbanecmbot/11bots/cswiki/userbots/patrolAutopatrolled
 echo 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user in (select distinct ug_user from user_groups where ug_group in ("autopatrolled", "bot", "sysop")) order by rc_timestamp desc' | sql cswiki | sed 1d > topatrol.txt
+python patrol.py
 cd $OLDPWD
