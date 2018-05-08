7639d7740055ce27fa877075c354ce71f8781832
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-08 13:16:48 +0200

    [patrolAutopatrolled] Fix path

--- cswiki/userbots/patrolAutopatrolled/run.sh
+++ cswiki/userbots/patrolAutopatrolled/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /data/project/urbanecmbot/patrolAutopatrolled
+cd /data/project/urbanecmbot/11bots/cswiki/userbots/patrolAutopatrolled
 echo 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user in (select distinct ug_user from user_groups where ug_group in ("autopatrolled", "bot", "sysop")) order by rc_timestamp desc' | sql cswiki | sed 1d > topatrol.txt
 cd $OLDPWD
