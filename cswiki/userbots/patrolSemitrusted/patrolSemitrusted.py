09be52adf3f3529d4bf7bfa21a2e4845ec5bd307
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-18 23:06:47 +0200

    +script

---
+++ cswiki/userbots/patrolSemitrusted/patrolSemitrusted.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from sseclient import SSEClient as EventSource
+import os
+import json
+import requests
+import pywikibot
+import logging
+
+stream = 'https://stream.wikimedia.org/v2/stream/recentchange'
+
+if __name__ == "__main__":
+	logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolSemitrusted.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+	try:
+		site = pywikibot.Site()
+		for event in EventSource(stream):
+			if event.event == 'message':
+				try:
+					change = json.loads(event.data)
+				except ValueError:
+					continue
+				if change['wiki'] == 'cswiki':
+					users = open('users.txt').read().split('\n')
+					if change['user'] in users:
+						list(site.patrol(rcid=change['id']))
+	except Exception as e:
+		logging.exception('Unknown exception occured')
---
+++ cswiki/userbots/patrolSemitrusted/patrolSemitrusted.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+source ~/11bots/cswiki/userbots/patrolSemitrusted/venv/bin/activate
