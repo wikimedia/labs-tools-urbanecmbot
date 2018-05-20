42c9b6c07070a568ee70c07947ade1c2356b4204
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-20 17:08:28 +0200

    RC_ID is wanted

--- cswiki/userbots/patrolSemitrusted/patrolSemitrusted.py
+++ cswiki/userbots/patrolSemitrusted/patrolSemitrusted.py
@@ -25,11 +25,11 @@ if __name__ == "__main__":
 					users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolSemitrusted/users.txt').read().split('\n')
 					if 'revision' in change:
 						if change['user'] in users:
-							logging.info('Making %s as patrolled', change['revision']['new'])
+							logging.info('Making %s as patrolled', change['id'])
 							logging.debug('Revision data=%s', change)
-							list(site.patrol(rcid=change['revision']['new']))
+							list(site.patrol(rcid=change['id']))
 						else:
-							logging.debug('Skipping %s', change['revision']['new'])
+							logging.debug('Skipping %s', change['id']])
 							logging.debug('Revision data=%s', change)
 	except Exception as e:
 		logging.exception('Unknown exception occured')
