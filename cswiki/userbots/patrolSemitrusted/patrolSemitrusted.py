0e54d0ae8936ddd39fdeaed9b3495c27616a7f5f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-20 16:57:40 +0200

    Better define what will happen after a to-be-patrolled edit will happen

--- cswiki/userbots/patrolSemitrusted/patrolSemitrusted.py
+++ cswiki/userbots/patrolSemitrusted/patrolSemitrusted.py
@@ -21,7 +21,13 @@ if __name__ == "__main__":
 					continue
 				if change['wiki'] == 'cswiki':
 					users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolSemitrusted/users.txt').read().split('\n')
-					if change['user'] in users:
-						list(site.patrol(rcid=change['id']))
+					if 'revision' in change:
+						if change['user'] in users:
+							logging.info('Making %s as patrolled', change['revision']['new'])
+							logging.debug('Revision data=%s', change)
+							list(site.patrol(rcid=change['revision']['new']))
+						else:
+							logging.debug('Skipping %s', change['revision']['new'])
+							logging.debug('Revision data=%s', change)
 	except Exception as e:
 		logging.exception('Unknown exception occured')
