619c75626e87b1e9ef68ee78de0306832c967d07
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-29 00:00:51 +0000

    Update cron.txt

--- cswiki/userbots/cleanSandbox/clean_and_patrol.py
+++ cswiki/userbots/cleanSandbox/clean_and_patrol.py
@@ -10,8 +10,12 @@ page = pywikibot.Page(site, u"Wikipedie:Pískoviště")
 page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor.txt').read()
 page.save('Robot: Úhrad pískoviště')
 
+page = pywikibot.Page(site, u"Šablona:Test")
+page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor_sablona.txt').read()
+page.save('Robot: Úhrad pískoviště')
+
 with conn.cursor() as cur:
-	cur.execute('select rc_id from revision join recentchanges on rc_this_oldid=rev_id where rev_page=3008 and rc_patrolled=0;')
+	cur.execute('select rc_id from revision join recentchanges on rc_this_oldid=rev_id where rev_page in (3008, 5264) and rc_patrolled=0;')
 	data = cur.fetchall()
 
 for row in data:
