a9fe38fc2eb98f8658b015634e5487383ce1d7a8
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-11 17:18:37 +0200

    Take patrolAutopatrolled from DB

--- cswiki/userbots/patrolAutopatrolled/patrol.py
+++ cswiki/userbots/patrolAutopatrolled/patrol.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+from wmflabs import db
+conn = db.connect('cswiki')
 import pywikibot
 site = pywikibot.Site()
 
-ids = open('topatrol.txt').readlines()
+
+ids = []
+with conn.cursor() as cur:
+	sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user in (select distinct ug_user from user_groups where ug_group in ("autopatrolled", "bot", "sysop")) order by rc_timestamp desc'
+	cur.execute(sql)
+	data = cur.fetchall()
+	for row in data:
+		ids.append(row[0])
+
+users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolSemitrusted/users.txt', 'r').read().split('\n')
+for user in users:
+	with conn.cursor() as cur:
+		sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user_text="%s"' % user
+		cur.execute(sql)
+		data = cur.fetchall()
+		for row in data:
+			ids.append(row[0])
 
 for id in ids:
-	id = int(id.replace('\n', ''))
 	try:
 		list(site.patrol(rcid=id))
 	except:
