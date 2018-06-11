46ab4f55b5dd07798ac8541cdba46c1d99d40445
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-11 17:33:56 +0200

    Semitrusted users should not patrol new pages

--- cswiki/userbots/patrolAutopatrolled/patrol.py
+++ cswiki/userbots/patrolAutopatrolled/patrol.py
@@ -18,7 +18,7 @@ with conn.cursor() as cur:
 users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolSemitrusted/users.txt', 'r').read().split('\n')
 for user in users:
 	with conn.cursor() as cur:
-		sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user_text="%s"' % user
+		sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user_text="%s" and rc_new=0' % user
 		cur.execute(sql)
 		data = cur.fetchall()
 		for row in data:
