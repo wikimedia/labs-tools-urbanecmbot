4210a229cd091975f5f481395d1189d5dba5914d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-12 16:55:46 +0200

    Remove old run.sh

--- cswiki/userbots/patrolAutopatrolled/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /data/project/urbanecmbot/11bots/cswiki/userbots/patrolAutopatrolled
-echo 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user in (select distinct ug_user from user_groups where ug_group in ("autopatrolled", "bot", "sysop")) order by rc_timestamp desc' | sql cswiki | sed 1d > topatrol.txt
-python patrol.py
-cd $OLDPWD
