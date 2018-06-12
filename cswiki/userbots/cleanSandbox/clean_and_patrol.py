eae6fe23e3a4af7a34458b247ef4f4fad486751a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-12 08:19:26 +0200

    Typo

--- cswiki/userbots/cleanSandbox/clean_and_patrol.py
+++ cswiki/userbots/cleanSandbox/clean_and_patrol.py
@@ -8,11 +8,11 @@ site = pywikibot.Site()
 
 page = pywikibot.Page(site, u"Wikipedie:Pískoviště")
 page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor.txt').read()
-page.save('Robot: Úhrad pískoviště')
+page.save('Robot: Úhrab pískoviště')
 
 page = pywikibot.Page(site, u"Šablona:Test")
 page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor_sablona.txt').read()
-page.save('Robot: Úhrad pískoviště')
+page.save('Robot: Úhrab pískoviště')
 
 with conn.cursor() as cur:
 	cur.execute('select rc_id from revision join recentchanges on rc_this_oldid=rev_id where rev_page in (3008, 5264) and rc_patrolled=0;')
