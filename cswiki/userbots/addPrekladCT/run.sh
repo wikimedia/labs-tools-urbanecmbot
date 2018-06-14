c23f84f9ba5c3c82c14f0dc8617e83c6f2529c3b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-14 21:16:01 +0200

    Fix path

--- cswiki/userbots/addPrekladCT/run.sh
+++ cswiki/userbots/addPrekladCT/run.sh
@@ -1,6 +1,6 @@
 export LC_ALL=en_US.UTF-8
 export PYTHONIOENCODING=utf8
-cd /data/project/urbanecmbot/addPrekladCT
+cd /data/project/urbanecmbot/11bots/cswiki/userbots/addPrekladCT
 sql cswiki < sql.sql | sed 1d > preklads.txt
 cp -r preklads.txt ~/tmpPublic/
 python generateExceptions.py > exceptions.txt
