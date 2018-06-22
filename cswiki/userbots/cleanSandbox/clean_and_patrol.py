26bddcc92de4999b1db081694e9f55c95afcf217
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-22 15:24:55 +0200

    Temprarily disable test template

--- cswiki/userbots/cleanSandbox/clean_and_patrol.py
+++ cswiki/userbots/cleanSandbox/clean_and_patrol.py
@@ -10,9 +10,9 @@ page = pywikibot.Page(site, u"Wikipedie:Pískoviště")
 page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor.txt').read()
 page.save('Robot: Úhrab pískoviště')
 
-page = pywikibot.Page(site, u"Šablona:Test")
-page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor_sablona.txt').read()
-page.save('Robot: Úhrab pískoviště')
+#page = pywikibot.Page(site, u"Šablona:Test")
+#page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor_sablona.txt').read()
+#page.save('Robot: Úhrab pískoviště')
 
 with conn.cursor() as cur:
 	cur.execute('select rc_id from revision join recentchanges on rc_this_oldid=rev_id where rev_page in (3008, 5264) and rc_patrolled=0;')
