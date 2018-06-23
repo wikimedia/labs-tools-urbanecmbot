41548bf44d019961f40a0f9a5b71f4eda25ce730
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-24 00:37:47 +0200

    Add emptyTalkpages

---
+++ cswiki/userbots/emptyTalkpages/emptyTalkpages.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import pywikibot
+import toolforge
+
+conn = toolforge.connect('cswiki')
+site = pywikibot.Site()
+
+with conn.cursor() as cur:
+	cur.execute('select page_title, page_namespace from page where page_len=0 and page_namespace in (1) and page_is_redirect=0;')
+	data = cur.fetchall()
+
+exceptions = pywikibot.Page(site, "User:UrbanecmBot/Záměrně prázdné diskusní stránky").text.split('\n')
+
+for row in data:
+	page = pywikibot.Page(site, row[0].decode('utf-8'), ns=row[1])
+	if page.title() in exceptions: continue
+	page.text = "{{smazat|prázdná diskusní stránka}}"
+	page.save(u"Robot: Smazat: Prázdná diskusní stránka")
---
+++ cswiki/userbots/emptyTalkpages/emptyTalkpages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/toolforge/bin/activate
+python ~/11bots/cswiki/userbots/emptyTalkpages/emptyTalkpages.py
