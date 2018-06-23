741d20981657b523fe9769b744ff4a3dc1499e0a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-24 00:37:37 +0200

    Add emptyCoursePages

---
+++ cswiki/userbots/emptyCoursePages/emptyCoursePages.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import pywikibot
+import toolforge
+
+conn = toolforge.connect('cswiki')
+site = pywikibot.Site()
+
+with conn.cursor() as cur:
+	cur.execute('select page_title from page where page_len=0 and page_namespace=4 and page_title like "Nástěnka/%";')
+	data = cur.fetchall()
+
+for row in data:
+	page = pywikibot.Page(site, row[0].decode('utf-8'), ns=4)
+	page.text = "{{smazat|prázdná stránka kurzu, kurz byl smazán na nástěnce}}"
+	page.save(u"Robot: Smazat: Prázdná stránka kurzu")
