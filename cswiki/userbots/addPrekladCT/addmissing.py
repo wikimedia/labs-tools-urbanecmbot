f553310ac9d035e0dcc72bdf60c9c527fffc860f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-28 00:06:06 +0200

    Rewrite addPrekladCT

--- cswiki/userbots/addPrekladCT/addmissing.py
+++ cswiki/userbots/addPrekladCT/addmissing.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import toolforge
+conn = toolforge.connect('cswiki')
 from pywikibot import textlib
 import re
 import pywikibot
@@ -9,20 +11,32 @@ site = pywikibot.Site()
 
 exceptions = ['comment', 'nowiki', 'pre']
 
-rows = open('preklads.txt', 'r').read().split('\n')
-for row in rows:
-	row = row.split('\t')
-	page = pywikibot.Page(site, row[0])
+# Get on wiki exeptions
+not_edit_page = pywikibot.Page(site, u'Wikipedista:UrbanecmBot/Nepřidávat šablonu překlad')
+not_edit_pages = not_edit_page.linkedPages()
+not_edit = []
+for page in not_edit_pages:
+	not_edit.append(page.title().replace(u' ', u'_'))
+
+# Get pages with no translation template
+with conn.cursor() as cur:
+	cur.execute(open('sql.sql').read())
+	data = cur.fetchall()
+
+for row in data:
+	if row[0] in not_edit: continue # Skip page if in exeptions
+	page = pywikibot.Page(site, row[0].decode('utf-8'))
+	if '{{překlad' in page.text.lower(): continue # Skip page if contain translation template
 	if re.search(r'(?i)== *Reference *==', page.text):
-		page.text = textlib.replaceExcept(page.text, r'(?i)==(=?) *Reference *===?\s*', r'==\1 Reference ==\1\n' + row[1] + r'\n', exceptions)
+		page.text = textlib.replaceExcept(page.text, r'(?i)==(=?) *Reference *===?\s*', r'==\1 Reference ==\1\n' + row[1].decode('utf-8') + r'\n', exceptions)
 	elif re.search(r'(?i)== *Poznámky *==', page.text):
-		page.text = textlib.replaceExcept(page.text, r'(?i)==(=?) *Poznámky *===?\s+(.*)\s*', r'==\1 Poznámky ==\1\n\2\n\n==\1 Reference ==\1\n' + row[1] + r'\n\n', exceptions)
+		page.text = textlib.replaceExcept(page.text, r'(?i)==(=?) *Poznámky *===?\s+(.*)\s*', r'==\1 Poznámky ==\1\n\2\n\n==\1 Reference ==\1\n' + row[1].decode('utf-8') + r'\n\n', exceptions)
 	elif re.search(r'(?i)== *(Literatura|Související články|Externí odkazy) *==', page.text):
-		page.text = textlib.replaceExcept(page.text, r'(?i)\s*==(=?) *(Literatura|Související články|Externí odkazy) *===?', r'\n\n==\1 Reference ==\1\n' + row[1] + r'\n\n==\1 \2 ==\1', exceptions, count=1)
+		page.text = textlib.replaceExcept(page.text, r'(?i)\s*==(=?) *(Literatura|Související články|Externí odkazy) *===?', r'\n\n==\1 Reference ==\1\n' + row[1].decode('utf-8') + r'\n\n==\1 \2 ==\1', exceptions, count=1)
 	elif re.search(r'(?i)\{\{(Autoritní data|Portály|DEFAULTSORT)[^\}]*\}\}', page.text):
-		page.text = textlib.replaceExcept(page.text, r'(?i)\s*\{\{((Autoritní data|Portály|DEFAULTSORT)[^\}]*)\}\}', r'\n\n== Reference ==\n' + row[1] + r'\n\n{{\1}}', exceptions, count=1)
+		page.text = textlib.replaceExcept(page.text, r'(?i)\s*\{\{((Autoritní data|Portály|DEFAULTSORT)[^\}]*)\}\}', r'\n\n== Reference ==\n' + row[1].decode('utf-8') + r'\n\n{{\1}}', exceptions, count=1)
 	elif re.search(r'(?i)\[\[Kategorie:[^\]]*\]\]', page.text):
-		page.text = textlib.replaceExcept(page.text, r'(?i)\s*\[\[Kategorie:([^\]]*)\]\]', r'\n\n== Reference ==\n' + row[1] + r'\n\n[[Kategorie:\1]]', exceptions, count=1)
+		page.text = textlib.replaceExcept(page.text, r'(?i)\s*\[\[Kategorie:([^\]]*)\]\]', r'\n\n== Reference ==\n' + row[1].decode('utf-8') + r'\n\n[[Kategorie:\1]]', exceptions, count=1)
 	else:
-		page.text = page.text + '\n\n== Reference ==\n' + row[1]
+		page.text = page.text + '\n\n== Reference ==\n' + row[1].decode('utf-8')
 	page.save('Robot: Přidání šablony Překlad')
--- cswiki/userbots/addPrekladCT/run.sh
+++ cswiki/userbots/addPrekladCT/run.sh
@@ -1,10 +1,5 @@
-export LC_ALL=en_US.UTF-8
-export PYTHONIOENCODING=utf8
-cd /data/project/urbanecmbot/11bots/cswiki/userbots/addPrekladCT
-sql cswiki < sql.sql | sed 1d > preklads.txt
-cp -r preklads.txt ~/tmpPublic/
-python generateExceptions.py > exceptions.txt
-cut -f 1 < preklads.txt | sort -u | grep -F -x -v -f exceptions.txt > pages.txt
-python3 addmissing.py -always -intersect -file:pages.txt -search:'-insource:/\{\{[Pp]řeklad/'
-rm *.txt
-cd $OLDPWD
+#!/bin/bash
+
+cd ~/11bots/cswiki/userbots/addPrekladCT
+source ~/toolforge/bin/activate
+python3 addmissing.py
