9cc5806c5305c2c7252260450662841c436244ee
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-29 09:01:38 +0200

    Rformat

--- cswiki/userbots/patrolTrusted/patrolDashboard.py
+++ cswiki/userbots/patrolTrusted/patrolDashboard.py
@@ -6,9 +6,7 @@ import pywikibot
 
 from datetime import timedelta, datetime
 
-logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolDashboard.log',
-		    level=logging.DEBUG,
-		    format='%(asctime)s %(levelname)s:%(message)s')
+logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolDashboard.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
 args = pywikibot.handle_args()
 
@@ -19,12 +17,10 @@ minutes = int(args[0]) if args and args[0].isdigit() else 15
 start = datetime.utcnow() - timedelta(minutes=minutes)
 starttime = start.strftime('%Y%m%d%H%M%S')
 
-for rev in site.recentchanges(
-        start=starttime, showBot=False, showPatrolled=False, reverse=True,
-        tag='OAuth CID: 1024'):
-    try:
-	p = site.patrol(revid=rev['revid'])
-	list(p)
-	logging.info('Marking revision %s as patrolled', rev['revid'])
-    except Exception as e:
-        logging.exception('Exception occured')
+for rev in site.recentchanges(start=starttime, showBot=False, showPatrolled=False, reverse=True, tag='OAuth CID: 1024'):
+	try:
+		p = site.patrol(revid=rev['revid'])
+		list(p)
+		logging.info('Marking revision %s as patrolled', rev['revid'])
+	except Exception as e:
+		logging.exception('Exception occured')
--- cswiki/userbots/patrolTrusted/patrolUndo.py
+++ cswiki/userbots/patrolTrusted/patrolUndo.py
@@ -6,9 +6,7 @@ import pywikibot
 
 from datetime import timedelta, datetime
 
-logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolUndo.log',
-		    level=logging.DEBUG,
-		    format='%(asctime)s %(levelname)s:%(message)s')
+logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolUndo.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
 args = pywikibot.handle_args()
 
@@ -19,12 +17,10 @@ minutes = int(args[0]) if args and args[0].isdigit() else 15
 start = datetime.utcnow() - timedelta(minutes=minutes)
 starttime = start.strftime('%Y%m%d%H%M%S')
 
-for rev in site.recentchanges(
-        start=starttime, showBot=False, showPatrolled=True, reverse=True,
-        tag='mw-undo'):
-    try:
-	p = site.patrol(revid=rev['old_revid'])
-	list(p)
-	logging.info('Marking revision %s as patrolled', rev['old_revid'])
-    except Exception as e:
-        logging.exception('Exception occured')
+for rev in site.recentchanges(start=starttime, showBot=False, showPatrolled=True, reverse=True, tag='mw-undo'):
+	try:
+		p = site.patrol(revid=rev['old_revid'])
+		list(p)
+		logging.info('Marking revision %s as patrolled', rev['old_revid'])
+	except Exception as e:
+		logging.exception('Exception occured')
