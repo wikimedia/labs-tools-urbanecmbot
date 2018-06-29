f70e96ea777e109e12a9bf5f46b6ed5fdec68431
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-29 09:17:17 +0200

    Add patrolRollback  again

---
+++ cswiki/userbots/patrolTrusted/patrolRollback.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
+import logging
+import pywikibot
+
+from datetime import timedelta, datetime
+
+logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolRollback.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+
+args = pywikibot.handle_args()
+
+site = pywikibot.Site()
+
+minutes = int(args[0]) if args and args[0].isdigit() else 15
+
+start = datetime.utcnow() - timedelta(minutes=minutes)
+starttime = start.strftime('%Y%m%d%H%M%S')
+
+for rev in site.recentchanges(start=starttime, showBot=False, showPatrolled=False, reverse=True, tag='mw-rollback'):
+	try:
+		p = site.patrol(revid=rev['revid'])
+		list(p)
+		logging.info('Marking revision %s as patrolled', rev['revid'])
+	except Exception as e:
+		logging.exception('Exception occured')
