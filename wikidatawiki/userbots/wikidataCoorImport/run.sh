f16cace0d6dab27efc364b16405ba21da608d58f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-12-03 21:17:26 +0100

    [wikidataCoordinateImports] Bugfix, path outside tools repository
    Change-Id: Ib43ab9ba7531b21c9e8c8d605ff6991d71650507

--- wikidatawiki/userbots/wikidataCoorImport/run.sh
+++ wikidatawiki/userbots/wikidataCoorImport/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/home/urbanecm/Documents/wikidatawiki/coorImport/basic.sh 'http://petscan.wmflabs.org/?language=cs&project=wikipedia&depth=10&categories=%C3%9Adr%C5%BEba%3ABez%20sou%C5%99adnic%20na%20Wikidatech&ns%5B0%5D=1&templates_any=Infobox%20-%20budova%0D%0AInfobox%20-%20kostel%0D%0AInfobox%20-%20s%C3%ADdlo%0D%0AInfobox%20-%20s%C3%ADdlo%20sv%C4%9Bta%0D%0AInfobox%20-%20vodn%C3%AD%20tok&interface_language=en&&doit=&format=tsv'
-/home/urbanecm/Documents/wikidatawiki/coorImport/basic.sh 'http://petscan.wmflabs.org/?language=sk&project=wikipedia&ns%5B0%5D=1&templates_any=Infobox%20Kostol%0D%0AInfobox%20s%C3%ADdlo&interface_language=en&&doit=&format=tsv' '-lang:sk'
+/data/project/urbanecmbot/11bots/wikidatawiki/coorImport/basic.sh 'http://petscan.wmflabs.org/?language=cs&project=wikipedia&depth=10&categories=%C3%9Adr%C5%BEba%3ABez%20sou%C5%99adnic%20na%20Wikidatech&ns%5B0%5D=1&templates_any=Infobox%20-%20budova%0D%0AInfobox%20-%20kostel%0D%0AInfobox%20-%20s%C3%ADdlo%0D%0AInfobox%20-%20s%C3%ADdlo%20sv%C4%9Bta%0D%0AInfobox%20-%20vodn%C3%AD%20tok&interface_language=en&&doit=&format=tsv'
+/data/project/urbanecmbot/11bots/wikidatawiki/coorImport/basic.sh 'http://petscan.wmflabs.org/?language=sk&project=wikipedia&ns%5B0%5D=1&templates_any=Infobox%20Kostol%0D%0AInfobox%20s%C3%ADdlo&interface_language=en&&doit=&format=tsv' '-lang:sk'
