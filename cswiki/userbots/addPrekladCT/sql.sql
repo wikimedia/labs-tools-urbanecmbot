1046c7d4a3ea6478af5e9cad5fab9e887f7e74ee
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-01-13 15:30:57 +0100

    [addPrekladCT] Change sql to match new database structure
    Change-Id: Ie2306108100598790c920d5955dd736b516feb1e

--- cswiki/userbots/addPrekladCT/sql.sql
+++ cswiki/userbots/addPrekladCT/sql.sql
@@ -1,14 +1,15 @@
 select page_title,
 concat(
 	"{{Překlad|",
-	left(replace(rev_comment, "Vytvořeno překladem stránky „[[:", ""), 2), "|",
-	replace(replace(replace(rev_comment, substring_index(rev_comment, "|", 1), ""), "|", ""), "]]“", ""), "|",
-	SUBSTRING_INDEX(replace(substr(replace(rev_comment, "Vytvořeno překladem stránky „[[:", ""), 4), "Special:Redirect/revision/", ""), "|", 1), "}}"
+	left(replace(comment_text, "Vytvořeno překladem stránky „[[:", ""), 2), "|",
+	replace(replace(replace(comment_text, substring_index(comment_text, "|", 1), ""), "|", ""), "]]“", ""), "|",
+	SUBSTRING_INDEX(replace(substr(replace(comment_text, "Vytvořeno překladem stránky „[[:", ""), 4), "Special:Redirect/revision/", ""), "|", 1), "}}"
 ) as template
 from change_tag
 join revision on ct_rev_id=rev_id
 join page on rev_page=page_id
-where ct_tag="contenttranslation"
+join comment on rev_comment_id=comment_id
+where ct_tag_id in (select ctd_id from change_tag_def where ctd_name in ("contenttranslation", "contenttranslation-v2"))
 and ct_rev_id is not null
 and rev_page not in
 (select tl_from from templatelinks where tl_title="Překlad")
