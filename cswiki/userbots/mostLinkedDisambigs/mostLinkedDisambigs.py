282d5f35a72937a5068e04c09a2cdc05a6748b53
Matěj Suchánek <matejsuchanek97@gmail.com>
2019-02-15 17:34:00 +0100

    mostLinkedDisambigs: Count only links from the main namespace
    Change-Id: Ib8e4a41107940ad4f07139d6b77db72d7ff5cf66

--- cswiki/userbots/mostLinkedDisambigs/mostLinkedDisambigs.py
+++ cswiki/userbots/mostLinkedDisambigs/mostLinkedDisambigs.py
@@ -8,7 +8,7 @@ from wmflabs import db
 import pywikibot
 site = pywikibot.Site()
 
-sql = 'select count(*), pl_title from pagelinks where pl_namespace=0 and pl_title in (select page_title from page where page_id in (select cl_from from categorylinks where cl_to="Wikipedie:Rozcestníky") and page_namespace=0) group by pl_title order by count(*) desc, pl_title limit 500;'
+sql = 'select count(*), pl_title from pagelinks where pl_namespace=0 and pl_from_namespace = 0 and pl_title in (select page_title from page where page_id in (select cl_from from categorylinks where cl_to="Wikipedie:Rozcestníky") and page_namespace=0) group by pl_title order by count(*) desc, pl_title limit 500;'
 conn = db.connect('cswiki')
 cur = conn.cursor()
 with cur:
