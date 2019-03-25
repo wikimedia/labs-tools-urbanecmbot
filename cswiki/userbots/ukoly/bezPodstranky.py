dfcbbb6a96e624fa1e149521dbdde31d0b9ca726
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-25 16:20:28 +0100

    [ukoly/bezPodstranky] Do not wait for replication
    Change-Id: I37b18ebfa7e03de9e64ece474508900fcecac2bf

--- cswiki/userbots/ukoly/bezPodstranky.py
+++ cswiki/userbots/ukoly/bezPodstranky.py
@@ -10,18 +10,6 @@ conn = toolforge.connect('cswiki', cluster='analytics')
 
 cur = conn.cursor()
 with cur:
-	sql = 'select page_title from categorylinks join page on cl_from=page_id where cl_to="Údržba:Šablona_Úkoly_bez_podstránky" and page_namespace%2=1'
-	cur.execute(sql)
-	data = cur.fetchall()
-
-for row in data:
-	page = pywikibot.Page(site, row[0].decode('utf-8'), ns=1)
-	page.purge()
-
-time.sleep(10) # Wait for replicating changed category to Toolsforge
-
-cur = conn.cursor()
-with cur:
 	sql = 'select page_title, page_namespace from categorylinks join page on cl_from=page_id where cl_to="Údržba:Šablona_Úkoly_bez_podstránky" and page_namespace%2=1'
 	cur.execute(sql)
 	data = cur.fetchall()
