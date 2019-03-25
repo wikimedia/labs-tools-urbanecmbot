b9af9e89012cc40ec436fa54bf5d9c37ce23e121
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-25 16:11:29 +0100

    [ukoly/bezPodstranky] Open page in correct namespace
    Do not hardcode namespace. Doing so without setting the premises in
    query can lead in editing incorrect titles.
    
    Change-Id: I56b142a59a0447264bf6f770307862c45a63b44c

--- cswiki/userbots/ukoly/bezPodstranky.py
+++ cswiki/userbots/ukoly/bezPodstranky.py
@@ -22,13 +22,13 @@ time.sleep(10) # Wait for replicating changed category to Toolsforge
 
 cur = conn.cursor()
 with cur:
-	sql = 'select page_title from categorylinks join page on cl_from=page_id where cl_to="Údržba:Šablona_Úkoly_bez_podstránky" and page_namespace%2=1'
+	sql = 'select page_title, page_namespace from categorylinks join page on cl_from=page_id where cl_to="Údržba:Šablona_Úkoly_bez_podstránky" and page_namespace%2=1'
 	cur.execute(sql)
 	data = cur.fetchall()
 
 for row in data:
-	page = pywikibot.Page(site, row[0].decode('utf-8'), ns=1)
-	text = page.text.encode('utf-8')
+	page = pywikibot.Page(site, row[0].decode('utf-8'), ns=row[1])
+	text = page.text
 	text = re.sub(r'{{Úkoly\|?[\d]*}}', '', text, flags=re.IGNORECASE)
 	if text.strip() == '':
 		page.delete(reason=u"Robot: Úkoly vyřešeny", mark=True)
