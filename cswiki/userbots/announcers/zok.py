2dcc4ac14f5362ae81e8abd0422fd802ff2108ae
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-03-31 14:27:22 +0200

    Add ZOK announcer
    Change-Id: I727fc448543a94f78d1585863f85f02c67c4bc31

---
+++ cswiki/userbots/announcers/zok.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+import pywikibot
+import time
+import sys
+import re
+
+import logging
+logger = logging.getLogger("pywiki")
+logger.setLevel(logging.WARNING)
+
+PAGE_PREFIX = "Wikipedie:Žádost o komentář"
+PAGE_LIST = "Wikipedie:Žádost o komentář"
+RE_SECTION = re.compile(r'^==[^=\n]+(|.*[^=\n]+)==\s*$', re.MULTILINE)
+RE_SUBPAGE = re.compile(r'\[\[/(?P<subpage>[^\}\n]+)/\]\]')
+
+def get_candidates(site):
+	page = pywikibot.Page(site, PAGE_LIST)
+	last_section = re.split(RE_SECTION, page.get())[-1]
+	out = []
+	for candidate in RE_SUBPAGE.finditer(last_section):
+		subpage_name = candidate.group('subpage')
+		out.append({
+			'PREFIX': PAGE_PREFIX,
+			'subpage_name': subpage_name,
+		})
+	return out
+
+ANNOUNCES = [
+	{'page':u'User:UrbanecmBot/ŽOK-OznámeníRC', 'big_tl':u'|- id="Sablona--OznameniRC__Oznameni_Diskuse"\n! [[Wikipedie:Žádost o komentář|Žádost o komentář]]:\n| %s\n', 'small_tl':u"[[%(PREFIX)s/%(subpage_name)s|%(subpage_name)s]]", 'separator':u' • ', 'empty':u'<!-- momentálně tu nic není -->'},
+	{'page':u'User:UrbanecmBot/ŽOK-PortálWP', 'big_tl':u'%s\n', 'small_tl':u"* [[%(PREFIX)s/%(subpage_name)s|%(subpage_name)s]]", 'separator':u'\n', 'empty':u'<!-- momentálně tu nic není -->'},
+]
+
+def announce_candidates(site, candidates):
+	for announce in ANNOUNCES:
+		if len(candidates) > 0:
+			items = [announce['small_tl'] % candidate for candidate in candidates]
+			inner_text = announce['separator'].join(items)
+			new_text = announce['big_tl'] % inner_text
+		else:
+			new_text = announce['empty']
+		page = pywikibot.Page(site, announce['page'])
+		if not page.exists() or new_text.strip() != page.get().strip():
+			comment = u'hlásič ŽOK hlásá (počet: %s)' % len(candidates)
+			page.put(new_text, comment=comment)
+		
+
+if __name__ == '__main__':
+	site = pywikibot.Site()
+	candidates = get_candidates(site)
+	announce_candidates(site, candidates)
+	pywikibot.stopme()
