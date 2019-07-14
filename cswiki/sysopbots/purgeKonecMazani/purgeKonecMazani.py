20101ae9cd14f3c47ae5ea27bde6f8ec654675b5
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-07-14 13:40:35 +0200

    Separate purgeKonecMazani to touch and purge actions
    Some pages must be touched in order to work correctly,
    some can be just purged.
    
    Change-Id: I12acc5cc4098f4196b13120466a6198785637d0c

--- cswiki/sysopbots/purgeKonecMazani/purgeKonecMazani.py
+++ cswiki/sysopbots/purgeKonecMazani/purgeKonecMazani.py
@@ -4,6 +4,10 @@
 import pywikibot
 site = pywikibot.Site()
 
-for page_title in ["MediaWiki:Konec mazání", "Wikipedie:Porušení práv/Články zřejmě porušující autorská práva"]:
+for page_title in ["MediaWiki:Konec mazání"]:
+	page = pywikibot.Page(site, page_title)
+	page.touch()
+
+for page_title in ["Wikipedie:Porušení práv/Články zřejmě porušující autorská práva"]:
 	page = pywikibot.Page(site, page_title)
 	page.purge()
