b6c1f0756f7ceeec078f96e44c589596756fdee2
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-09-29 14:40:05 +0200

    Reflect actor migration in autopatrol.py
    Change-Id: I6e336db8a6130873c7e8f97ab6c304991f28bb48

--- cswiki/userbots/patrolTrusted/autopatrol.py
+++ cswiki/userbots/patrolTrusted/autopatrol.py
@@ -11,7 +11,7 @@ logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolAutopatrol.lo
 
 ids = []
 with conn.cursor() as cur:
-	sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user in (select distinct ug_user from user_groups where ug_group in ("autopatrolled", "bot", "sysop")) order by rc_timestamp desc'
+	sql = 'select rc_id from recentchanges join actor on rc_actor=actor_id where rc_namespace!=8 and rc_patrolled=0 and actor_user!=0 and actor_name in (select distinct ug_user from user_groups where ug_group in ("autopatrolled", "bot", "sysop")) order by rc_timestamp desc'
 	cur.execute(sql)
 	data = cur.fetchall()
 	for row in data:
@@ -20,7 +20,7 @@ with conn.cursor() as cur:
 users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolTrusted/editpatrol.txt', 'r').read().split('\n')
 for user in users:
 	with conn.cursor() as cur:
-		sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user_text="%s" and rc_new=0' % user
+		sql = 'select rc_id from recentchanges join actor on rc_actor=actor_id where rc_namespace!=8 and rc_patrolled=0 and actor_user!=0 and actor_name="%s" and rc_new=0' % user
 		cur.execute(sql)
 		data = cur.fetchall()
 		for row in data:
@@ -29,7 +29,7 @@ for user in users:
 users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolTrusted/trustedpatrol.txt', 'r').read().split('\n')
 for user in users:
 	with conn.cursor() as cur:
-		sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user_text="%s" and (rc_new=0 or rc_namespace!=0)' % user
+		sql = 'select rc_id from recentchanges join actor on rc_actor=actor_id where rc_namespace!=8 and rc_patrolled=0 and actor_user!=0 and actor_name="%s" and (rc_new=0 or rc_namespace!=0)' % user
 		cur.execute(sql)
 		data = cur.fetchall()
 		for row in data:
@@ -38,7 +38,7 @@ for user in users:
 users = open('/data/project/urbanecmbot/11bots/cswiki/userbots/patrolTrusted/pagepatrol.txt', 'r').read().split('\n')
 for user in users:
 	with conn.cursor() as cur:
-		sql = 'select rc_id from recentchanges where rc_namespace!=8 and rc_patrolled=0 and rc_user_text="%s"' % user
+		sql = 'select rc_id from recentchanges join actor on rc_actor=actor_id where rc_namespace!=8 and rc_patrolled=0 and actor_user!=0 and actor_name="%s"' % user
 		cur.execute(sql)
 		data = cur.fetchall()
 		for row in data:
