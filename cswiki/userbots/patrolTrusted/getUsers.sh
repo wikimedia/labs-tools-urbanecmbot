acf4c1c3608382424820382c8330a3735bda8f26
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-01-26 17:29:20 +0100

    patrolTrusted/getUsers: Ignore trailing spaces
    Change-Id: Ib3f54430e307d16bd28709e1e0bbe441a211fe1b

--- cswiki/userbots/patrolTrusted/getUsers.sh
+++ cswiki/userbots/patrolTrusted/getUsers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl 'https://cs.wikipedia.org/wiki/Wikipedista:UrbanecmBot/EditPatrol?action=raw' 2> /dev/null | grep -vE '</?pre>' > /data/project/urbanecmbot/11bots/cswiki/userbots/patrolTrusted/editpatrol.txt
+curl 'https://cs.wikipedia.org/wiki/Wikipedista:UrbanecmBot/EditPatrol?action=raw' 2> /dev/null | grep -vE '</?pre>' | sed 's/ *$//g;s/^ *//g' > /data/project/urbanecmbot/11bots/cswiki/userbots/patrolTrusted/editpatrol.txt
