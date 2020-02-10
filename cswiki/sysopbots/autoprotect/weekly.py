e0e997d8e0ffb5499c0918dca07ed31640b0dfb2
Zoranzoki21 <zorandori4444@gmail.com>
2020-02-10 21:45:48 +0000

    Fix E101 issue found by flake8
    Bug: T237256
    Change-Id: Ibaa6a2f669579ce812c3a5fd9af7d7704735763f

--- cswiki/sysopbots/autoprotect/weekly.py
+++ cswiki/sysopbots/autoprotect/weekly.py
@@ -7,18 +7,18 @@ yearweek = d.isocalendar()[:2]
 # ^ tohle nám dá dvojici (rok, týden)
 
 pages = [ # různé články pro tento týden
-        u'Wikipedie:Článek týdne/%04d/%02d' % yearweek,
-        u'Wikipedie:Obrázek týdne/%04d/%02d' % yearweek,
-        u'Wikipedie:Zajímavosti/%04d/%02d' % yearweek,
+	u'Wikipedie:Článek týdne/%04d/%02d' % yearweek,
+	u'Wikipedie:Obrázek týdne/%04d/%02d' % yearweek,
+	u'Wikipedie:Zajímavosti/%04d/%02d' % yearweek,
 ]
 
 site = pywikibot.getSite()
 for pgname in pages: # pro každou stránku ze seznamu
-        page = pywikibot.Page(site, pgname) # najdu ji na wiki
-        if page.exists():
-	        page.protect( # a zamknu...
-	            reason='automatizovany zamek neceho tydne', # popis editace
-		    protections={'edit': 'autoconfirmed', 'move': 'sysop'},
-		    expiry='170 hours'
-	        )
+	page = pywikibot.Page(site, pgname) # najdu ji na wiki
+	if page.exists():
+		page.protect( # a zamknu...
+			reason='automatizovany zamek neceho tydne', # popis editace
+			protections={'edit': 'autoconfirmed', 'move': 'sysop'},
+			expiry='170 hours'
+		)
 pywikibot.stopme() # dáme prostor i ostatním
