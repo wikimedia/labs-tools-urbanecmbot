24b2ef7abfbf394b1dca93780d3e033c5eefd61b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-02-23 16:01:04 +0100

    [mostLinkedRedirs] Convert to Python3
    Change-Id: I2a7001c0622e12d957237ccc6780413cab146d14

--- cswiki/userbots/mostLinkedRedirs/mostLinkedRedirs.py
+++ cswiki/userbots/mostLinkedRedirs/mostLinkedRedirs.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-import sys
 from wmflabs import db
 import pywikibot
-reload(sys)
-sys.setdefaultencoding('utf-8')
+
 site = pywikibot.Site()
 
 sql = 'select count(*), pl_title from pagelinks where pl_namespace=0 and pl_title in (select page_title from page where page_id in (select rd_from from redirect) and page_namespace=0) group by pl_title order by count(*) desc, pl_title limit 500;'
@@ -17,10 +15,10 @@ with cur:
 
 res = u""
 for row in data:
-	rowres = u"# [[" + row[1].encode('utf-8') + u"]] ([[Speciální:Co odkazuje na/" + row[1].encode('utf-8') + u"|" + unicode(row[0]) + u" odkazů]])"
+	rowres = "# [[" + row[1].decode('utf-8') + "]] ([[Speciální:Co odkazuje na/" + row[1].decode('utf-8') + "|" + row[0] + " odkazů]])"
 	res += rowres + '\n'
 
 
-page = pywikibot.Page(site, u"Wikipedie:Údržbové seznamy/Nejvíce odkazovaná přesměrování/seznam")
+page = pywikibot.Page(site, "Wikipedie:Údržbové seznamy/Nejvíce odkazovaná přesměrování/seznam")
 page.text = res
-page.save(u'Robot: Aktualizace seznamu nejodkazovanějších přesměrování')
+page.save('Robot: Aktualizace seznamu nejodkazovanějších přesměrování')
