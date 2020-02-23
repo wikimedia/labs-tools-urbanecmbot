eb20626c99c45944720ebce56ccd051534b98df9
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-02-23 22:39:39 +0100

    [badProtectTemplates] Migrate to Python3
    Change-Id: Ic54c3aaac53f41e89629ed5cb059dc5db33edc3f

--- cswiki/userbots/badProtectTemplates/badprotecttemplates.sh
+++ cswiki/userbots/badProtectTemplates/badprotecttemplates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ~/pwb/scripts/blockpageschecker.py -always
-python ~/pwb/scripts/blockpageschecker.py -catr:"Údržba:Stránky s chybným použitím šablony o zamčení" -always
+python3 ~/pwb/scripts/blockpageschecker.py -always
+python3 ~/pwb/scripts/blockpageschecker.py -catr:"Údržba:Stránky s chybným použitím šablony o zamčení" -always
