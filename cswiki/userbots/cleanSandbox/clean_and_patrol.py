4b4ebd5445780bcc72f505e68dc44e9a5d9202f5
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-03-22 14:09:44 +0100

    [cleanSandbox] Clean sandbox only if it is old enough
    Change-Id: I4002b22c1f284d5927bc7a62f61bfdc0c539c3e7

--- cswiki/userbots/cleanSandbox/clean_and_patrol.py
+++ cswiki/userbots/cleanSandbox/clean_and_patrol.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+from datetime import datetime
 import toolforge
 import pywikibot
 conn = toolforge.connect('cswiki')
 site = pywikibot.Site()
 
 page = pywikibot.Page(site, u"Wikipedie:Pískoviště")
-page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor.txt').read()
-page.save('Robot: Úhrab pískoviště')
+if (datetime.now() - page.editTime()).seconds / 60 / 60 > 2:
+	page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor.txt').read()
+	page.save('Robot: Úhrab pískoviště')
 
-# page = pywikibot.Page(site, u"Šablona:Test")
-# page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor_sablona.txt').read()
-# page.save('Robot: Úhrab pískoviště')
+page = pywikibot.Page(site, u"Šablona:Test")
+if (datetime.now() - page.editTime()).seconds / 60 / 60 > 8:
+	page.text = open('/data/project/urbanecmbot/11bots/cswiki/userbots/cleanSandbox/vzor_sablona.txt').read()
+	page.save('Robot: Úhrab pískoviště')
 
 with conn.cursor() as cur:
 	cur.execute('select rc_id from revision join recentchanges on rc_this_oldid=rev_id where rev_page in (3008, 5264) and rc_patrolled=0;')
