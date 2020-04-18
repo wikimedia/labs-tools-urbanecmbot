84afef59b52c781ef5e832b37ad65a9326dbfe3b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-04-18 19:30:31 +0200

    Add deletedEverywhere
    Change-Id: Ic9f8d2abe9add440ed73fecd1a462889b710b7fa

---
+++ metawiki/deletedEverywhere/deletedEverywhere.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import toolforge
+import pywikibot
+
+site = pywikibot.Site('meta', 'meta')
+
+conn = toolforge.connect('wikidatawiki')
+with conn.cursor() as cur:
+	cur.execute('select ips_site_id, ips_site_page from wb_items_per_site where ips_item_id=5964')
+	speedy_items = cur.fetchall()
+
+result = """{| class="wikitable sortable"
+|+
+!Project
+!Number of sysops
+!Link to category
+!Number of items
+"""
+
+for speedy_item in speedy_items:
+	conn = toolforge.connect(speedy_item[0].decode('utf-8'))
+	with conn.cursor() as cur:
+		cur.execute('select count(*) from categorylinks where cl_to=%s', speedy_item[1].decode('utf-8').split(':')[1].replace(' ', '_'))
+		numOfItems = cur.fetchall()[0][0]
+	with conn.cursor() as cur:
+		cur.execute('select count(*) from user_groups where ug_group="sysop";')
+		numOfSysops = cur.fetchall()[0][0]
+	conn = toolforge.connect('meta')
+	with conn.cursor() as cur:
+		cur.execute('select url from wiki where dbname=%s', speedy_item[0].decode('utf-8'))
+		url = cur.fetchall()[0][0]
+	result += "|-\n"
+	result += "|%s\n" % speedy_item[0].decode('utf-8')
+	result += "|%d\n" % numOfSysops
+	result += "|[%s/wiki/%s %s]\n" % (url, speedy_item[1].decode('utf-8').replace(' ', '_'), speedy_item[1].decode('utf-8'))
+	result += "|%d\n" % numOfItems
+
+result += "|}"
+
+page = pywikibot.Page(site, "User:UrbanecmBot/Wiki speedy delete")
+page.text = result
+page.save('Bot: Publish wiki speedy delete report')
---
+++ metawiki/deletedEverywhere/deletedEverywhere.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/toolforge/bin/activate
+python ~/11bots/metawiki/deletedEverywhere/deletedEverywhere.py
