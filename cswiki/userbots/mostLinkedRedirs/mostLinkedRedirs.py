9cfb42a5139bed7c727505424057b032d4d4394f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-05-09 14:59:05 +0200

    mostLinkedRedirs: Make the script work
    Change-Id: Ifccdcf80d90a5069cbba3eae3cd5b7a1e6de3543

--- cswiki/userbots/mostLinkedRedirs/mostLinkedRedirs.py
+++ cswiki/userbots/mostLinkedRedirs/mostLinkedRedirs.py
@@ -6,7 +6,7 @@ import pywikibot
 
 site = pywikibot.Site()
 
-sql = 'select count(*), pl_title from pagelinks where pl_namespace=0 and pl_title in (select page_title from page where page_id in (select rd_from from redirect) and page_namespace=0) group by pl_title order by count(*) desc, pl_title limit 500;'
+sql = 'select count(*), pl_title from pagelinks where pl_namespace=0 and pl_title in (select page_title from page where page_id in (select rd_from from redirect) and page_namespace=0) group by pl_title order by count(*) desc, pl_title limit 100;'
 conn = db.connect('cswiki')
 cur = conn.cursor()
 with cur:
@@ -15,7 +15,7 @@ with cur:
 
 res = u""
 for row in data:
-	rowres = "# [[" + row[1].decode('utf-8') + "]] ([[Speciální:Co odkazuje na/" + row[1].decode('utf-8') + "|" + row[0] + " odkazů]])"
+	rowres = "# [[" + row[1].decode('utf-8') + "]] ([[Speciální:Co odkazuje na/" + row[1].decode('utf-8') + "|" + str(row[0]) + " odkazů]])"
 	res += rowres + '\n'
 
 
