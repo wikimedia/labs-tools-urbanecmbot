4ec5fa24cabfe334161456623f63716b2098cc55
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-07-17 21:39:50 +0200

    editpatrolMaintainer: Remove autopatrolled users
    Change-Id: Ie0f21d02d2e6e973e6adc889d8c73d8f01f4d712

--- cswiki/sysopbots/editpatrolMaintainer/editpatrolMaintainer.py
+++ cswiki/sysopbots/editpatrolMaintainer/editpatrolMaintainer.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import toolforge
 import pywikibot
+
 site = pywikibot.Site()
+conn = toolforge.connect('cswiki', cluster='analytics')
+
+with conn.cursor() as cur:
+	cur.execute('select user_name from user_groups join user on ug_user=user_id where ug_group="autopatrolled"')
+	data = cur.fetchall()
+
+autopatrolled = []
+for row in data:
+	autopatrolled.append(row[0].decode('utf-8'))
 
 page = pywikibot.Page(site, 'Wikipedista:UrbanecmBot/EditPatrol')
-users = page.text.split('\n')
-users.pop(0)
-users.pop()
-users.sort()
+usersOrig = page.text.split('\n')
+usersOrig.pop(0)
+usersOrig.pop()
+usersOrig.sort()
+
+users = []
+for user in usersOrig:
+	if user not in autopatrolled:
+		users.append(user)
+
 page.text = '<pre>\n' + '\n'.join(users) + '\n</pre>'
-page.save('Robot: Seřazení seznamu')
+page.save('Robot: Seřazení seznamu, odebrání prověřených uživatelů')
---
+++ cswiki/sysopbots/editpatrolMaintainer/editpatrolMaintainer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/toolforge/bin/activate
+sysopbot /data/project/urbanecmbot/11bots/cswiki/sysopbots/editpatrolMaintainer/editpatrolMaintainer.py
