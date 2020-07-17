8861089985937e432622e25ba68cf34823075fc0
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-07-17 18:24:01 +0200

    emptyCoursePages: Do not ask for confirmation
    Change-Id: I637594039f8f9313bc2f579646c0361b625582a8

--- cswiki/sysopbots/emptyCoursePages/emptyCoursePages.py
+++ cswiki/sysopbots/emptyCoursePages/emptyCoursePages.py
@@ -15,4 +15,4 @@ for row in data:
 	page = pywikibot.Page(site, row[0].decode('utf-8'), ns=4)
 	if not page.exists():
 		continue
-	page.delete("prázdná stránka kurzu, kurz byl smazán na nástěnce", mark=True)
+	page.delete("Robot: prázdná stránka kurzu, kurz byl smazán na nástěnce", mark=True, prompt=False)
