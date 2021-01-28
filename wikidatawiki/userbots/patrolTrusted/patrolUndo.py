89bc064d92665fae98e4fc6911117cb9bbcea499
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-01-28 19:14:17 +0100

    Add wikidata version of patrolUndo
    Change-Id: Ia46ad406b92429c45030202881154409bdf53411

---
+++ wikidatawiki/userbots/patrolTrusted/patrolUndo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
+import logging
+import pywikibot
+
+from datetime import timedelta, datetime
+
+#logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolUndo.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+
+args = pywikibot.handle_args()
+
+site = pywikibot.Site('wikidata', 'wikidata')
+
+minutes = int(args[0]) if args and args[0].isdigit() else 3600
+
+start = datetime.utcnow() - timedelta(minutes=minutes)
+starttime = start.strftime('%Y%m%d%H%M%S')
+
+for rev in site.recentchanges(start=starttime, showBot=False, showPatrolled=False, reverse=True, tag='mw-reverted'):
+	try:
+		p = site.patrol(revid=rev['revid'])
+		list(p)
+		#logging.info('Marking revision %s as patrolled', rev['revid'])
+	except Exception:
+		pass
