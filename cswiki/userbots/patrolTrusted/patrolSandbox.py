d1ad9bf6d055208fc016c1473f4c92f7298fe56a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-12-27 15:01:41 +0100

    patrolSandbox: Use Pywikibot EventStreams
    Change-Id: I4043c8f4e804ca69f5beb92fb3320c3882354f60

--- cswiki/userbots/patrolTrusted/patrolSandbox.py
+++ cswiki/userbots/patrolTrusted/patrolSandbox.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from sseclient import SSEClient as EventSource
+from pywikibot.comms.eventstreams import EventStreams
 import os
 import json
 import requests
@@ -8,22 +8,18 @@ import pywikibot
 import logging
 import toolforge
 
-stream = 'https://stream.wikimedia.org/v2/stream/recentchange'
-
 if __name__ == "__main__":
 	logging.basicConfig(filename='/data/project/urbanecmbot/logs/patrolSandbox.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 	try:
 		site = pywikibot.Site()
-		for event in EventSource(stream):
-			if event.event == 'message':
-				try:
-					change = json.loads(event.data)
-				except ValueError:
-					continue
-				if change['wiki'] == 'cswiki' and 'revision' in change and not change['patrolled']:
-					logging.info("Processing change %s", json.dumps(change))
-					if change["title"].startswith("Wikipedista:%s" % change['user']) and 'old' in change['length']:
-						logging.info('Marking %s as patrolled, because it was made in personal user space', change['id'])
-						list(site.patrol(rcid=change['id']))
+		stream = EventStreams(streams=['recentchange'])
+		stream.register_filter(server_name='cs.wikipedia.org', type='edit')
+		while True:
+			change = next(iter(stream))
+			if 'revision' in change and not change['patrolled']:
+				logging.info("Processing change %s", json.dumps(change))
+				if change["title"].startswith("Wikipedista:%s" % change['user']) and 'old' in change['length']:
+					logging.info('Marking %s as patrolled, because it was made in personal user space', change['id'])
+					list(site.patrol(rcid=change['id']))
 	except Exception:
 		logging.exception('Unknown error occured')
