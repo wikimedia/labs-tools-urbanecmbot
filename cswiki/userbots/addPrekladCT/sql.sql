b6c165d317def2d1f1a38670b4022c04f3db6767
Martin Urbanec <martin.urbanec@wikimedia.cz>
2022-10-25 22:18:33 +0200

    addPreklad: Update to use linktarget
    Change-Id: I97a856dbd308343fff8a40cdbae1a7bece56af02

--- cswiki/userbots/addPrekladCT/sql.sql
+++ cswiki/userbots/addPrekladCT/sql.sql
@@ -7,9 +7,9 @@ join comment on rev_comment_id=comment_id
 where ct_tag_id in (select ctd_id from change_tag_def where ctd_name in ("contenttranslation", "contenttranslation-v2"))
 and ct_rev_id is not null
 and rev_page not in
-(select tl_from from templatelinks where tl_title="Překlad")
+(select tl_from from templatelinks join linktarget on lt_id=tl_target_id where lt_title="Překlad")
 and page_namespace=0
 /*and rev_parent_id=0*/
-and rev_page not in (select tl_from from templatelinks where tl_title like "Rozcestník%")
+and rev_page not in (select tl_from from templatelinks join linktarget on lt_id=tl_target_id where lt_title like "Rozcestník%")
 and left(replace(comment_text, "Vytvořeno překladem stránky „[[:", ""), 2)!="cs"
 and page_is_redirect=0;
