419cefaaf1f0f6069ee7a3cf4ad24d996c2d5193
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-05-28 12:58:46 +0200

    Fix a couple of scripts
    Change-Id: I588b588888d985323eff9c9976750343a5601668

--- cswiki/userbots/badProtectTemplates/badprotecttemplates.sh
+++ cswiki/userbots/badProtectTemplates/badprotecttemplates.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/venv/bin/activate
+
 #python3 ~/pwb/scripts/blockpageschecker.py -always
 #python3 ~/pwb/scripts/blockpageschecker.py -catr:"Údržba:Stránky s chybným použitím šablony o zamčení" -always
 python3 ~/pwb/scripts/blockpageschecker.py -catr:"Údržba:Stránky s chybným použitím šablony o zamčení" -ns:0 -always
--- cswiki/userbots/markStudents/cache.sh
+++ cswiki/userbots/markStudents/cache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source ~/11bots/cswiki/userbots/markStudents/venv/bin/activate
+source ~/venv/bin/activate
 python3 ~/11bots/cswiki/userbots/markStudents/cache.py
