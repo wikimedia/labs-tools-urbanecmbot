cd375e08209d55085d96a05f97b3aa1d8b75f05d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-11-23 23:06:43 +0100

    deletedEverywhere: Add minor updates
    Change-Id: Ied72aafc1295d2a6aafabe56aadaf8655aca0903

--- metawiki/deletedEverywhere/deletedEverywhere.py
+++ metawiki/deletedEverywhere/deletedEverywhere.py
@@ -4,6 +4,7 @@
 import toolforge
 import pywikibot
 import requests
+from datetime import datetime
 
 site = pywikibot.Site('meta', 'meta')
 
@@ -28,6 +29,7 @@ result = """{| class="wikitable sortable"
 !Link to category
 !Number of items
 """
+resultCSV = 'Project\tNumber of sysops\tLink to category\tNumber of items\tIs GS?\n'
 
 totalCSD = 0
 for speedy_item in speedy_items:
@@ -66,13 +68,18 @@ for speedy_item in speedy_items:
 	result += "|{{subst:formatnum:%d}}\n" % numOfSysops
 	result += "|[%s/wiki/%s %s]\n" % (url, speedy_item[1].decode('utf-8').replace(' ', '_'), speedy_item[1].decode('utf-8'))
 	result += "|{{subst:formatnum:%d}}\n" % numOfItems
+	resultCSV += '\t'.join([speedy_item[0].decode('utf-8'), str(numOfSysops), speedy_item[1].decode('utf-8'), str(numOfItems), str(dbname in non_gs_wikis)])
 	totalCSD += numOfItems
+
+with open('/data/project/urbanecmbot/public_html/deleted-everywhere/data-%s.tsv' % datetime.now().strftime('%Y%m%d%H%M%S'), 'w') as f:
+	f.write(resultCSV)
 
 result += """|+
 ! colspan="3" |Total
 !{{subst:formatnum:%d}}
 """ % totalCSD
-result += "|}"
+result += "|}\n"
+result += "<small>Available as TSV: [https://urbanecmbot.toolforge.org/deleted-everywhere/ download]</small>"
 
 page = pywikibot.Page(site, "Global sysops/Speedy delete requests/Data")
 page.text = result
